cedcf0201588429a6f4254fb6441349010cb9e23
Carlos <carlosmcejas@gmail.com>
2023-12-09 19:57:24 -0500

    actually works nicely now

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-import tkinter as tk
-import PIL
-from PIL import ImageEnhance
-from tkinter import filedialog
-from PIL import Image, ImageTk
+from tkinter import filedialog, Tk, Canvas, Button, Scrollbar, BOTH
+from PIL import Image, ImageTk, ImageEnhance
 
-# function to open a file dialog and select an image
+# global variables to store the original image, the Tkinter image, and the select button
+original_image = None
+tk_image = None
+select_button = None
+
+# function to select an image
 def select_image():
-    # open the file dialog
+    global original_image, select_button
+
+    # open a file dialog to select the image
     path = filedialog.askopenfilename()
     if not path:
         return
@@ -15,66 +19,90 @@ def select_image():
     image = Image.open(path)
 
     # deep fry the image using PIL
-    image = deep_fry(image)
+    original_image = deep_fry(image)
 
     # display the fried image using Tkinter
-    display_image(image)
+    display_image(original_image)
+
+    # hide the select button
+    select_button.place_forget()
 
 # function to deep fry the given image using PIL
 def deep_fry(image):
     # deep fry the image using PIL
-    converter = PIL.ImageEnhance.Color(image)
+    converter = ImageEnhance.Color(image)
     converter.enhance(5).save("sat-deepfry-image.png") # saturates
-    image = PIL.Image.open('sat-deepfry-image.png')
-    converter = PIL.ImageEnhance.Sharpness(image)
+    image = Image.open('sat-deepfry-image.png')
+    converter = ImageEnhance.Sharpness(image)
     converter.enhance(15).save('comp-image.png') # sharpens
-    image = PIL.Image.open('comp-image.png')
+    image = Image.open('comp-image.png')
 
     return image
 
 # function to display the given image using Tkinter
 def display_image(image):
-    # convert the image to Tkinter format
-    image = ImageTk.PhotoImage(image)
+    global tk_image
 
-    # create a canvas with a scrollbar
-    canvas = tk.Canvas(root, width=500, height=500)
-    canvas.pack(side="left", fill="both", expand=True)
+    # calculate the aspect ratio of the image
+    aspect_ratio = image.width / image.height
 
-    scrollbar = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
-    scrollbar.pack(side="right", fill="y")
+    # calculate the new dimensions of the image
+    new_width = min(image.width, 800)
+    new_height = int(new_width / aspect_ratio)
 
-    canvas.configure(yscrollcommand=scrollbar.set)
-    canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    # resize the image for display purposes
+    display_image = image.resize((new_width, new_height))
 
-    # display the image in the canvas
-    canvas_image = canvas.create_image(0, 0, anchor="nw", image=image)
+    # convert the resized image to Tkinter format
+    tk_image = ImageTk.PhotoImage(display_image)
+
+    # adjust the size of the canvas to match the image
+    canvas.config(width=new_width, height=new_height)
+
+    # display the resized image in the canvas
+    canvas_image = canvas.create_image(0, 0, anchor="nw", image=tk_image)
     canvas.config(scrollregion=canvas.bbox("all"))
 
     # add a button to save the image
-    save_button = tk.Button(canvas, text="Save", command=lambda: save_image(image))
+    save_button = Button(canvas, text="Download", command=save_image)
     canvas.create_window(10, 10, anchor="nw", window=save_button)
 
+    # adds a scrollbar
+    canvas_image = canvas.create_image(0, 0, anchor="nw", image=tk_image)
+    canvas.config(scrollregion=canvas.bbox("all"))
+
 # function to save the given image
-def save_image(image):
+def save_image():
+    global original_image
+
     # open a file dialog to select the save path
     path = filedialog.asksaveasfilename(defaultextension=".png")
     if not path:
         return
 
     # save the image using PIL
-    image.save(path)
-
+    original_image.save(path)
 
 # create the root window
-root = tk.Tk()
+root = Tk()
 
-# set the size of the window to 500x500 pixels
-root.geometry("500x500")
+# set the size of the window to 800x500 pixels
+root.geometry("800x500")
+
+# create a canvas to display the image
+canvas = Canvas(root)
+canvas.pack(side="left", fill="both", expand=True)
+
+# creates image scrollbar
+scrollbar = Scrollbar(root, command=canvas.yview)
+scrollbar.pack(side="right", fill="y")
+
+# configure the canvas to scroll with the scrollbar
+canvas.config(yscrollcommand=scrollbar.set)
 
 # add a button to select an image
-select_button = tk.Button(root, text="Select Image", command=select_image)
-select_button.pack()
+select_button = Button(root, text="Select Image", command=select_image)
+select_button.place(x=1, y=1)
 
-# start the Tk
+# start the Tkinter main loop
 root.mainloop()
